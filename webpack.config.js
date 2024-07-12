a802b42ca936465eb35dd5f3051478efb023c81e
Michael Sullivan <sullivanmichael344@gmail.com>
2024-07-12 00:26:02 -0400

    Setup files and configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        filename: 'index.html',
+        inject: 'body',
+        template: './src/index.html',
+
+    }
+
+    )
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  
+};
